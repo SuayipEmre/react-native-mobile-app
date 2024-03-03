0f40bacdbb52584611ed3f41f8245eda339df0fe
Suayip Emre <suayipemredev@gmail.com>
2024-03-04 01:49:23 +0300

    feat: camera permission

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 import store from './src/store/app';
 import RootNavigator from './src/navigators/rootNavigator';
