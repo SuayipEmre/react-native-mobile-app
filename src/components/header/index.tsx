b2058bd17dece8ee0c78363b6d6aec6a007313f6
Suayip Emre <suayipemredev@gmail.com>
2024-03-07 22:25:56 +0300

    Header  were customized according to the pages

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,21 +7,27 @@ import Ant from 'react-native-vector-icons/AntDesign'
 import SearchInput from '../searchInput'
 import { useGenresModalVisible } from '../../store/features/modals/genres/hooks'
 import GenresModal from '../genresModal'
+import { colors } from '../../styles/colors'
+import { NavigationProp, useNavigation } from '@react-navigation/native'
+import { MainNavigatorStackParamList } from '../../navigators/types'
 
 
 type headerPropsType = {
-    isShowingCategoryButton?: boolean
     activeScreen: 'Home' | 'Tv' | 'Movie'
 }
 
-const Header: React.FC<headerPropsType> = ({ isShowingCategoryButton, activeScreen }) => {
+const Header: React.FC<headerPropsType> = ({  activeScreen }) => {
 
     const [isSearch, setIsSearch] = useState<boolean>(false)
+    const navigation = useNavigation<NavigationProp<MainNavigatorStackParamList>>()
 
     const isModalVisible = useGenresModalVisible()
+
+
+    const goHomeButton = () => <Ant name='closecircleo' size={24} color={colors.primary} onPress={() => navigation.navigate('HomeScreen')} />
+
     return (
         <View>
-
             <View style={styles.header_container}>
 
                 <View style={styles.title_container}>
@@ -31,9 +37,9 @@ const Header: React.FC<headerPropsType> = ({ isShowingCategoryButton, activeScre
 
                 {
                     isSearch && <SearchInput
-                     placeholder={activeScreen == 'Tv' ? 'Search A TV Show' : activeScreen == 'Home' ? 'Search something' :  'Search a movie'}
-                     activeSearchContent = {activeScreen}
-                     />
+                        placeholder={activeScreen == 'Tv' ? 'Search A TV Show' : activeScreen == 'Home' ? 'Search something' : 'Search a movie'}
+                        activeSearchContent={activeScreen}
+                    />
                 }
 
 
@@ -44,10 +50,26 @@ const Header: React.FC<headerPropsType> = ({ isShowingCategoryButton, activeScre
             </View>
 
             <View style={styles.bottom_content}>
-                <HeaderItem text='Movies' isGenreModalButton={false} />
-                <HeaderItem text='TV-Series' isGenreModalButton={false} />
                 {
-                    activeScreen == 'Tv' && <HeaderItem text='Categories' modalVisible={isModalVisible} isGenreModalButton />
+                    activeScreen == 'Movie' ? (
+                        <>
+                            {goHomeButton()}
+                            <HeaderItem text='TV-Series' isGenreModalButton={false} />
+                            <HeaderItem text='Categories' modalVisible={isModalVisible} isGenreModalButton />
+                        </>
+                    ) : activeScreen == 'Tv' ? (
+                        <>
+                            {goHomeButton()}
+                            <HeaderItem text='Movies' isGenreModalButton={false} />
+                            <HeaderItem text='Categories' modalVisible={isModalVisible} isGenreModalButton />
+                        </>
+                    ) :
+                        activeScreen == 'Home' && (
+                            <>
+                                <HeaderItem text='Movies' isGenreModalButton={false} />
+                                <HeaderItem text='TV-Series' isGenreModalButton={false} />
+                            </>
+                        )
                 }
             </View>
 
